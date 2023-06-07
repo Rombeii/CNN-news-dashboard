b6659c9fee57c5cec3526663be8ca29b23c1b6e2
Tamas Tardi <rombeii@freemail.hu>
2023-06-07 14:44:45 +0200

    split tabs into classes

--- docs/app/main.js
+++ docs/app/main.js
@@ -15,7 +15,7 @@ async function startApplication() {
   self.pyodide.globals.set("sendPatch", sendPatch);
   console.log("Loaded!");
   await self.pyodide.loadPackage("micropip");
-  const env_spec = ['markdown-it-py<3', 'https://cdn.holoviz.org/panel/1.1.0/dist/wheels/bokeh-3.1.1-py3-none-any.whl', 'https://cdn.holoviz.org/panel/1.1.0/dist/wheels/panel-1.1.0-py3-none-any.whl', 'pyodide-http==0.2.1', 'pandas']
+  const env_spec = ['markdown-it-py<3', 'https://cdn.holoviz.org/panel/1.1.0/dist/wheels/bokeh-3.1.1-py3-none-any.whl', 'https://cdn.holoviz.org/panel/1.1.0/dist/wheels/panel-1.1.0-py3-none-any.whl', 'pyodide-http==0.2.1', 'dateLayout', 'pandas']
   for (const pkg of env_spec) {
     let pkg_name;
     if (pkg.endsWith('.whl')) {
@@ -52,40 +52,7 @@ import pandas as pd
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, HoverTool, DatetimeTickFormatter
 
-def update_data(event):
-    start, end = pd.Timestamp(date_start.value), pd.Timestamp(date_end.value)
-
-    if start > end:
-        date_start.value, date_end.value = date_end.value, date_start.value
-    else:
-        filtered_occurrences = occurrences_by_date[
-            (pd.to_datetime(occurrences_by_date["publication_date"]) >= start) &
-            (pd.to_datetime(occurrences_by_date["publication_date"]) <= end)
-        ]
-        source.data = dict(
-            publication_date=pd.to_datetime(filtered_occurrences["publication_date"]),
-            count=filtered_occurrences["count"]
-        )
-
-        p.x_range.start = filtered_occurrences["publication_date"].min()
-        p.x_range.end = filtered_occurrences["publication_date"].max()
-
-        update_statistics(filtered_occurrences)
-
-
-def update_statistics(filtered_occurrences):
-    total_articles = filtered_occurrences["count"].sum()
-    min_articles = filtered_occurrences["count"].min()
-    min_articles_date = filtered_occurrences.loc[filtered_occurrences["count"].idxmin(), "publication_date"]
-    max_articles = filtered_occurrences["count"].max()
-    max_articles_date = filtered_occurrences.loc[filtered_occurrences["count"].idxmax(), "publication_date"]
-    avg_articles = filtered_occurrences["count"].mean()
-
-    total_occurrences_pane.object = f"**Total Published Articles:** {total_articles}"
-    min_occurrences_pane.object = f"**Minimum Published Articles:** {min_articles} (Date: {min_articles_date})"
-    max_occurrences_pane.object = f"**Maximum Published Articles:** {max_articles} (Date: {max_articles_date})"
-    average_occurrences_pane.object = f"**Average Published Articles:** {avg_articles:.2f}"
-
+from dateLayout import create_tab1_content
 
 pn.extension(sizing_mode="stretch_width", template="fast")
 
@@ -96,87 +63,22 @@ try:
 except FileNotFoundError:
     csv_url = "https://raw.githubusercontent.com/Rombeii/CNN-news-dashboard/main/extended_dataset.csv"
     data = pd.read_csv(csv_url)
-filtered_data = data.copy()
 
-# Filter out rows with unknown publication dates
-filtered_data = filtered_data[filtered_data["publication_date"] != "Unknown"]
 
-# Convert publication_date column in filtered_data to datetime
-filtered_data["publication_date"] = pd.to_datetime(filtered_data["publication_date"], errors="coerce").dt.date
+tab2_content = pn.pane.Markdown("Content for Tab 2")  # Placeholder content for Tab 2
 
-# Group by publication date and count occurrences
-occurrences_by_date = filtered_data.groupby("publication_date").size().reset_index(name="count")
-
-# Create a complete range of dates
-date_range = pd.date_range(start=filtered_data["publication_date"].min(), end=filtered_data["publication_date"].max(), freq="D")
-
-# Create a DataFrame with all dates
-all_dates = pd.DataFrame({"publication_date": date_range})
-
-# Convert publication_date column in all_dates to date
-all_dates["publication_date"] = pd.to_datetime(all_dates["publication_date"]).dt.date
-
-# Merge occurrences_by_date with all_dates using merge
-occurrences_by_date = all_dates.merge(occurrences_by_date, on="publication_date", how="left")
-occurrences_by_date["count"] = occurrences_by_date["count"].fillna(0)
-
-# Find the date with the highest number of published articles
-max_occurrences_date = occurrences_by_date.loc[occurrences_by_date["count"].idxmax(), "publication_date"]
-max_occurrences_count = occurrences_by_date["count"].max()
-
-# Find the date with the lowest number of published articles
-min_occurrences_date = occurrences_by_date.loc[occurrences_by_date["count"].idxmin(), "publication_date"]
-min_occurrences_count = occurrences_by_date["count"].min()
-
-# Calculate the total number of articles
-total_articles = occurrences_by_date["count"].sum()
-
-date_start = pn.widgets.DateSlider(name='Start Date', start=filtered_data["publication_date"].min(),
-                                   end=filtered_data["publication_date"].max(), value=filtered_data["publication_date"].min())
-date_end = pn.widgets.DateSlider(name='End Date', start=filtered_data["publication_date"].min(),
-                                 end=filtered_data["publication_date"].max(), value=filtered_data["publication_date"].max())
-
-date_start.param.watch(update_data, "value")
-date_end.param.watch(update_data, "value")
-
-p = figure(title="Number of Published Articles by Date", x_axis_label='Date',
-           y_axis_label='Number of Published Articles', width=800, height=400)
-source = ColumnDataSource(occurrences_by_date)
-p.line(x='publication_date', y='count', source=source, line_width=2)
-p.xaxis.formatter = DatetimeTickFormatter()
-
-hover_tool = HoverTool(tooltips=[("Date", "@publication_date{%F}"), ("Number of Articles", "@count")],
-                       formatters={"@publication_date": "datetime"})
-
-p.add_tools(hover_tool)
-
-total_occurrences_pane = pn.pane.Markdown()
-min_occurrences_pane = pn.pane.Markdown()
-max_occurrences_pane = pn.pane.Markdown()
-average_occurrences_pane = pn.pane.Markdown()
-
-update_statistics(occurrences_by_date)
-
-chart = pn.pane.Bokeh(p, sizing_mode="stretch_width")
-statistics = pn.Column(
-    pn.Row(total_occurrences_pane),
-    pn.Row(min_occurrences_pane),
-    pn.Row(max_occurrences_pane),
-    pn.Row(average_occurrences_pane),
-)
-
-layout = pn.Column(
-    pn.Row(date_start, date_end),
-    pn.Row(chart, statistics),
+tabs = pn.Tabs(
+    ("Date published", create_tab1_content(data)),
+    ("Tab 2", tab2_content),
 )
 
 # For development purposes
 # if __name__.startswith("bokeh"):
 #     # Start with: panel serve main.py --show
-#     app = layout.servable()
+#     app = tabs.servable()
 #     app.show(port=5007)
 
-app = layout.servable()
+app = tabs.servable()
 
 
 await write_doc()
